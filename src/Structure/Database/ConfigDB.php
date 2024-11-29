c366edf7b4c26c65934d8969cf8a5223c4664a3c
Gabriel Carlos <gabrielcss.oficial@gmail.com>
2024-11-29 12:31:44 -0300

    fix: remove unused files

--- src/Structure/Database/ConfigDB.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("DB_NAME", "users");
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "Absoluto-123");
--- src/Structure/Email/ConfigEmail.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-define("SMTP_CLIENT", 're_X5b7QQZq_Da7eUsujJnr1cir4vAjtDLV5');
-define("SMTP_FROM", 'Contato ADO <contato@gabrielcss.com.br>');
